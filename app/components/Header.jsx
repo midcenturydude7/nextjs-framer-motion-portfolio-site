9cb7f935bdf71d8e0e20ad25304afa9d0738a507
midCenturyDude7 <mgriffes@gmail.com>
2024-05-29 21:47:48 -0700

    Set up logo placeholder

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,11 @@
 import Menu from "./Menu";
 import Icons from "./Icons";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between border-b-[1px] border-slate-200/10 px-10 py-[3rem] shadow-xl">
+      <Logo />
       <Menu />
       <Icons />
     </div>
