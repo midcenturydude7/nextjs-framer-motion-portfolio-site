25e72da2420e7a913fdf74886223113d2ab30e4d
midCenturyDude7 <mgriffes@gmail.com>
2024-06-03 21:46:28 -0700

    Experimented with different states and styles/animations

--- app/components/MenuButton.jsx
+++ app/components/MenuButton.jsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import { navItems } from "../lib/navItems";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
@@ -11,6 +12,7 @@ export default function MenuButton({
   setFocused,
 }) {
   const pathname = usePathname();
+  const [background, setBackground] = React.useState(null);
 
   return (
     <div className="group relative flex gap-4 rounded-lg border border-transparent bg-zinc-900/90 pb-[0.65rem] pl-[.55rem] pr-[.55rem] pt-[0.75rem] leading-none transition-colors duration-1000 hover:border hover:border-zinc-100/30 hover:bg-black/80">
@@ -18,15 +20,15 @@ export default function MenuButton({
         const isActive = path === pathname;
         return (
           <Link key={id} href={path}>
-            <motion.button
-              layout
+            <button
               data-active={isActive}
               onClick={() => setSelected(path)}
               onKeyDown={(e) => (e.key === "Enter" ? setSelected(path) : null)}
               onFocus={() => setFocused(path)}
               onMouseEnter={() => setFocused(path)}
+              onMouseDown={() => setBackground(path)}
               tabIndex={0}
-              className={`relative flex h-[2.313rem] w-[7rem] items-center justify-center rounded-lg bg-slate-900/80 text-[#E8E8FD]/50 outline-[none] transition-colors focus:rounded-lg group-hover:bg-slate-900/50 ${selected === path ? "cursor-default bg-slate-700/40 text-[#fad4fe]/70 transition-colors duration-1000 hover:text-[#fad4fe]/70 group-hover:bg-slate-700/40" : "cursor-pointer hover:text-purple-200/90"}`}
+              className={`relative flex h-[2.313rem] w-[7rem] items-center justify-center rounded-lg bg-slate-900/80 text-[#E8E8FD]/50 outline-[none] transition-colors focus:rounded-lg group-hover:bg-slate-900/50 ${selected === path ? "cursor-default text-[#fad4fe]/70 transition-colors duration-1000 hover:text-[#fad4fe]/70" : "cursor-pointer hover:text-purple-200/90"} ${background === path ? "bg-[rgba(94,106,130,0.5)] group-hover:bg-[rgba(88,104,137,0.5)]" : ""}`}
             >
               <span className="absolute left-[1.25rem] top-1 stroke-1 transition-colors group-hover:stroke-2">
                 {svg}
@@ -60,12 +62,12 @@ export default function MenuButton({
               <AnimatePresence>
                 {selected === path ? (
                   <motion.div
-                    className="absolute bottom-[-10px] left-0 z-0 h-[4px] w-[100%] rounded-[8px] bg-slate-700/50 transition-colors duration-1000 group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600"
+                    className="absolute bottom-[-10px] left-0 z-0 h-[4px] w-[100%] rounded-[8px] bg-[rgba(94,106,130,0.4)] transition-colors duration-1000 group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600"
                     layoutId="underline"
                   />
                 ) : null}
               </AnimatePresence>
-            </motion.button>
+            </button>
           </Link>
         );
       })}
