644a44379838c34da0e38da9266a0f72a3ab9049
midCenturyDude7 <mgriffes@gmail.com>
2024-06-13 21:34:23 -0700

    Playing with background color styles

--- app/components/Icons.jsx
+++ app/components/Icons.jsx
@@ -27,10 +27,10 @@ export default function Icons() {
               <AnimatePresence>
                 {circle === label ? (
                   <motion.div
-                    className="absolute inset-[0.5px] z-0 h-[100%] w-[100%] rounded-full bg-[rgba(57,63,73,0.75)]"
+                    className="absolute inset-[0.5px] z-0 h-[100%] w-[100%] rounded-full bg-slate-700/80"
                     transition={{
                       layout: {
-                        duration: 0.4,
+                        duration: 0.375,
                         ease: "easeOut",
                       },
                     }}
--- app/components/MenuButton.jsx
+++ app/components/MenuButton.jsx
@@ -16,7 +16,7 @@ export default function MenuButton({
   const pathname = usePathname();
 
   return (
-    <div className="group relative flex gap-4 rounded-lg border border-transparent bg-zinc-900/90 pb-[0.65rem] pl-[.55rem] pr-[.55rem] pt-[0.75rem] leading-none transition-colors duration-1000 hover:border hover:border-zinc-100/30 hover:bg-black/80">
+    <div className="group relative z-0 flex gap-4 rounded-lg border border-transparent bg-zinc-900/90 pb-[0.65rem] pl-[.55rem] pr-[.55rem] pt-[0.75rem] leading-none transition-colors duration-1000 hover:border hover:border-zinc-100/30 hover:bg-black/80">
       {navItems.map(({ svg, path, id, label }) => {
         const isActive = path === pathname;
         return (
@@ -29,7 +29,7 @@ export default function MenuButton({
               onPointerEnter={() => setFocused(path)}
               onPointerUp={() => setBackground(path)}
               tabIndex={0}
-              className={`relative flex h-[2.313rem] w-[7rem] items-center justify-center rounded-lg bg-slate-900/80 text-[#E8E8FD]/50 outline-[none] transition-colors focus:rounded-[8px] group-hover:bg-slate-900/50 ${selected === path ? "cursor-default bg-slate-700/50 text-[#fad4fe]/70 transition-colors duration-1000 hover:text-[#fad4fe]/70" : "cursor-pointer duration-500 hover:text-purple-200/90"}`}
+              className={`relative flex h-[2.313rem] w-[7rem] items-center justify-center rounded-lg bg-slate-900/80 text-[#E8E8FD]/50 outline-[none] transition-colors focus:rounded-[8px] group-hover:bg-slate-900/50 ${selected === path ? "cursor-default bg-pink-800/80 text-[#fad4fe]/70 transition-colors duration-1000 hover:text-[#fad4fe]/70" : "cursor-pointer duration-500 hover:text-purple-200/90"}`}
             >
               <span className="absolute left-[1.25rem] top-1 stroke-1 transition-colors hover:duration-500 group-hover:stroke-2">
                 {svg}
@@ -42,10 +42,10 @@ export default function MenuButton({
               <AnimatePresence>
                 {focused === path ? (
                   <motion.div
-                    className={`absolute inset-0 z-0 h-[100%] w-[100%] rounded-[8px] bg-slate-700/50 ${background === path ? "bg-slate-700/50 transition-colors duration-1000" : ""}`}
+                    className={`absolute inset-0 z-0 h-[100%] w-[100%] rounded-[8px] bg-slate-700/80 ${background === path ? "bg-slate-700/50 transition-colors duration-1000" : ""}`}
                     transition={{
                       layout: {
-                        duration: 0.4,
+                        duration: 0.375,
                         ease: "easeOut",
                       },
                     }}
@@ -62,18 +62,22 @@ export default function MenuButton({
               </AnimatePresence>
               {selected === path ? (
                 <motion.div
-                  className="absolute bottom-[-10px] left-0 z-0 h-[4px] w-[100%] rounded-[8px] bg-slate-700/50 transition-colors duration-1000 group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600"
+                  className="absolute bottom-[-10px] left-0 z-0 h-[4.5px] w-[100%] rounded-[8px] bg-fuchsia-300/15 transition-colors duration-1000 group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600"
                   layoutId="underline"
                 />
               ) : null}
               <AnimatePresence>
                 {!focused && selected === path ? (
                   <motion.div
-                    className={`absolute inset-0 z-0 rounded-[8px] bg-slate-700/50 transition-colors duration-500`}
+                    className={`absolute inset-0 z-0 rounded-[8px] bg-slate-700/80 transition-colors duration-500`}
                     transition={{
                       layout: {
-                        duration: 0.4,
+                        duration: 0.25,
                         ease: "easeOut",
+                        type: "spring",
+                        bounce: 2,
+                        damping: 35,
+                        stiffness: 700,
                       },
                     }}
                     exit={{
