fffa85f14c7e19e5e8af42829e946a8a70325807
midCenturyDude7 <mgriffes@gmail.com>
2024-06-15 16:53:06 -0700

    Updated highlight with bg-gradient

--- app/components/Icons.jsx
+++ app/components/Icons.jsx
@@ -27,7 +27,7 @@ export default function Icons() {
               <AnimatePresence>
                 {circle === label ? (
                   <motion.div
-                    className="absolute inset-[0.5px] z-0 h-[100%] w-[100%] rounded-full bg-pink-600/80"
+                    className="absolute inset-[0.5px] z-0 h-[100%] w-[100%] rounded-full bg-gradient-to-r from-pink-600/70 to-purple-500/90"
                     transition={{
                       layout: {
                         duration: 0.375,
--- app/components/MenuButton.jsx
+++ app/components/MenuButton.jsx
@@ -46,7 +46,7 @@ export default function MenuButton({
                 {focused === path ? (
                   <motion.div
                     initial={false}
-                    className={`absolute inset-0 z-[2] h-[100%] w-[100%] rounded-[8px] bg-pink-600/80 transition-colors ${background === path ? "bg-fuchsia-700/30 transition-colors duration-1000" : ""}`}
+                    className={`absolute inset-0 z-[2] h-[100%] w-[100%] rounded-[8px] bg-gradient-to-r from-pink-600/70 to-purple-500/90 transition-colors ${background === path ? "bg-fuchsia-700/30 transition-colors duration-1000" : ""}`}
                     transition={{
                       layout: {
                         duration: 0.375,
@@ -67,7 +67,7 @@ export default function MenuButton({
               </AnimatePresence>
               {selected === path ? (
                 <motion.div
-                  className="absolute bottom-[-9px] left-0 z-0 h-[4.5px] w-[100%] rounded-[8px] bg-fuchsia-800/25 transition-colors duration-700 group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600 group-hover:duration-1000"
+                  className="absolute bottom-[-9px] left-0 z-0 h-[4.5px] w-[100%] rounded-[8px] bg-fuchsia-800/30 transition-colors duration-700 group-hover:bg-gradient-to-r group-hover:from-pink-600/80 group-hover:to-purple-500/90 group-hover:duration-1000"
                   transition={{
                     layout: {
                       duration: 0.25,
@@ -84,7 +84,7 @@ export default function MenuButton({
               <AnimatePresence>
                 {!focused && selected === path ? (
                   <motion.div
-                    className={`absolute inset-0 z-[2] rounded-[8px] bg-fuchsia-700/30 transition-colors duration-500 group-hover:bg-pink-600/80 group-hover:duration-1000`}
+                    className={`absolute inset-0 z-[2] rounded-[8px] bg-fuchsia-700/30 transition-colors duration-500 group-hover:bg-pink-600/70 group-hover:duration-1000`}
                     transition={{
                       layout: {
                         duration: 0.2,
