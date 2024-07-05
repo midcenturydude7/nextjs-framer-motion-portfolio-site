adbe66f77bfb00652c0966d13d1995fd8162d5af
midCenturyDude7 <mgriffes@gmail.com>
2024-07-04 21:42:24 -0700

    Updated layout

--- app/components/HomeWrapper.jsx
+++ app/components/HomeWrapper.jsx
@@ -3,13 +3,21 @@ import React from "react";
 export default function HomeWrapper() {
   return (
     <div className="flex h-lvh justify-center p-24">
-      <div className="flex h-[25%] w-[50%] flex-wrap items-stretch">
-        <div className="flex gap-8 w-1/3 flex-col">
-          <div className="flex-1 bg-red-500">Row 1</div>
-          <div className="flex-1 bg-green-500">Row 2</div>
-          <div className="flex-1 bg-blue-500">Row 3</div>
+      <div className="flex h-[33%] w-[45%]">
+        <div className="flex w-[55%] flex-col gap-[1.2rem]">
+          <div className="flex-1 rounded-bl-lg rounded-tl-lg border-t-[1px] border-l-[1px] border-slate-50/10 bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
+            <h1 className="">DESIGN</h1>
+          </div>
+          <div className="flex-1 rounded-bl-lg rounded-tl-lg bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
+            <h1>ANIMATION</h1>
+          </div>
+          <div className="flex-1 rounded-bl-lg rounded-tl-lg bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
+            <h1>DATA VISUALIZATION</h1>
+          </div>
         </div>
-        <div className="w-2/3 bg-yellow-500">Right side content</div>
+        <div className="w-[45%] bg-transparent pl-10 pt-4">
+          Right side content
+        </div>
       </div>
     </div>
   );
