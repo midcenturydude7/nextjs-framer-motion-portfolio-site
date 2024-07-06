a75214b6d3a4c2472780b1bff5707cd2ca7eb5bb
midCenturyDude7 <mgriffes@gmail.com>
2024-07-06 14:16:06 -0700

    Updated element positioning and styles

--- app/components/HomeWrapper.jsx
+++ app/components/HomeWrapper.jsx
@@ -5,18 +5,18 @@ export default function HomeWrapper() {
     <div className="flex h-lvh justify-center p-24">
       <div className="flex h-[33%] w-[45%]">
         <div className="flex w-[55%] flex-col gap-[1.2rem]">
-          <div className="flex-1 rounded-bl-lg rounded-tl-lg border-t-[1px] border-l-[1px] border-slate-50/10 bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
+          <div className="flex flex-1 items-center rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/10 bg-gradient-to-r from-slate-900/90 to-transparent pl-10">
             <h1 className="">DESIGN</h1>
           </div>
-          <div className="flex-1 rounded-bl-lg rounded-tl-lg bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
-            <h1>ANIMATION</h1>
+          <div className="flex flex-1 items-center rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/10 bg-gradient-to-r from-slate-900/90 to-transparent pl-10">
+            <h1>CODE</h1>
           </div>
-          <div className="flex-1 rounded-bl-lg rounded-tl-lg bg-gradient-to-r from-slate-900/90 to-transparent pl-10 pt-4">
-            <h1>DATA VISUALIZATION</h1>
+          <div className="flex flex-1 items-center rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/10 bg-gradient-to-r from-slate-900/90 to-transparent pl-10">
+            <h1>BUILD</h1>
           </div>
         </div>
-        <div className="w-[45%] bg-transparent pl-10 pt-4">
-          Right side content
+        <div className="flex w-[45%] flex-1 items-start justify-center rounded-lg border-r-[1px] border-t-[1px] border-slate-50/10 bg-transparent pl-10 pt-4">
+          <h1 className="text-6xl">HELLO.</h1>
         </div>
       </div>
     </div>
