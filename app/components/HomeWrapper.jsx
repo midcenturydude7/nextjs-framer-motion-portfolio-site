bcde5cb594e927454478732ad6bcc090f04a9e79
midCenturyDude7 <mgriffes@gmail.com>
2024-07-07 21:22:24 -0700

    Updated intro text

--- app/components/HomeWrapper.jsx
+++ app/components/HomeWrapper.jsx
@@ -72,13 +72,9 @@ export default function HomeWrapper() {
           <h1 className="inline-block bg-gradient-to-r from-sky-300/90 to-fuchsia-700/30 bg-clip-text font-[dolce,_sans-serif] text-[6.5rem] font-[800] not-italic text-transparent antialiased">
             Hello.
           </h1>
-          <p className="text-justif mt-[-1.75rem] text-[1.1rem] leading-8">
-            I am a self-taught frontend engineer with a passion for transforming
-            ideas into engaging, responsive, and high-performance websites. My
-            background in communications and my interest in and experience
-            working with Nextjs, React, and Framer-motion enable me to blend
-            creativity and technical expertise to create web solutions that
-            connect with users.
+          <p className="mt-[-1.75rem] text-justify text-[1.1rem] font-light leading-[2.2rem]">
+            Thanks for stopping by. I am a self-taught frontend engineer with a
+            background in communications. I have a passion for building web solutions that are engaging and useful. Take a moment to click around and let me know what you think.
           </p>
         </div>
       </div>
