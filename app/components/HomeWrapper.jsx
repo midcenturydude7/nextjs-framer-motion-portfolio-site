d34bc8fde798602a81b8dc38608579da51836cf1
midCenturyDude7 <mgriffes@gmail.com>
2024-07-08 20:50:50 -0700

    Working with new elements and fine-tuning color

--- app/components/HomeWrapper.jsx
+++ app/components/HomeWrapper.jsx
@@ -71,15 +71,25 @@ export default function HomeWrapper() {
           </div>
         </div>
         <div className="mt-[-4rem] flex w-[45%] flex-1 flex-col items-start justify-start rounded-lg border-r-[1px] border-t-[1px] border-transparent bg-transparent pl-[4rem]">
-          <h1 className="inline-block bg-gradient-to-r from-sky-300/90 to-fuchsia-700/30 bg-clip-text font-[dolce,_sans-serif] text-[6.5rem] font-[800] not-italic text-transparent antialiased">
-            Hello.
-          </h1>
-          <p className="mt-[-1.75rem] text-justify text-[1.1rem] font-light leading-[2.2rem]">
-            Thanks for stopping by. I am a self-taught frontend engineer with a
-            background in communications. I have a passion for building web
-            solutions that are engaginging and useful. Take a moment to click
-            around and let me know what you think.
-          </p>
+          <div className="flex w-full items-center justify-between">
+            <h1 className="inline-block bg-gradient-to-r from-sky-300/90 to-fuchsia-700/30 bg-clip-text font-[dolce,_sans-serif] text-[6.5rem] font-[800] not-italic text-transparent antialiased">
+              Hello.
+            </h1>
+            <div className="group relative mt-[-2.75rem] pr-12">
+              {/* <div className="absolute -inset-0.5 h-[7rem] w-[7rem] animate-tilt rounded-full bg-gradient-to-r from-sky-500/80 from-10% via-fuchsia-700/70 via-60% to-purple-800/90 to-80% opacity-10 blur transition duration-1000 group-hover:opacity-20" /> */}
+              <div className="flex h-[7.75rem] w-[7.75rem] items-center justify-center rounded-full border border-slate-200/10">
+                <p>I am Matt</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="mt-[-1.75rem] text-justify text-[1.1rem] font-light leading-[2.2rem]">
+              Thanks for stopping by. I am a self-taught frontend engineer with
+              a background in communications. I have a passion for building web
+              solutions that are engaginging and useful. Take a moment to click
+              around and let me know what you think.
+            </p>
+          </div>
         </div>
       </div>
       {/* MAIN CONTENT: PROJECTS SECTION */}
--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,7 +1,9 @@
 export default function Logo() {
   return (
-    <div className="inline-block bg-gradient-to-r from-sky-300/70 to-fuchsia-600/40 bg-clip-text font-[dolce,_sans-serif] text-[2.25rem] font-[900] not-italic text-transparent antialiased ">
-      mg(...dev)
+    <div className="flex items-center justify-end">
+      <div className="inline-block bg-gradient-to-r from-sky-300/70 to-fuchsia-600/40 bg-clip-text font-[dolce,_sans-serif] text-[2.25rem] font-[900] not-italic text-transparent antialiased ">
+        <h1>mg(...dev)</h1>
+      </div>
     </div>
   );
 }
