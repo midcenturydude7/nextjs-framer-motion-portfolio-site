03594bf1b57c509d86513e3a14bb8185aa49d98b
midCenturyDude7 <mgriffes@gmail.com>
2024-07-12 20:56:10 -0700

    Created iterable tabs and updated hover/select states

--- app/components/HomeWrapper.jsx
+++ app/components/HomeWrapper.jsx
@@ -1,14 +1,17 @@
 "use client";
 import React from "react";
+import { heroTabs } from "../lib/heroTabs";
 import { cn } from "../lib/utils";
 
 export default function HomeWrapper() {
   const [hovered, setHovered] = React.useState(false);
+  const [selected, setSelected] = React.useState(null);
 
   return (
     <div className="relative flex h-lvh flex-col items-center justify-start p-24">
       {/* ANIMATED SHAPES | TODO: CREATE COMPONENT FOR SCREEN READERS TO IGNORE */}
       <div className="absolute left-[63%] top-28 z-[-2] h-60 w-36 -rotate-45 rounded-xl bg-gradient-to-r from-slate-200/80 from-10% via-purple-900/90 via-50% to-fuchsia-700/70 to-90% opacity-30 blur-3xl" />
+      {/* BACKGROUND ANIMATION */}
       <div
         className={cn(
           "absolute left-[35%] top-20 z-[-1] h-36 w-72 rounded-full bg-sky-200/80 opacity-30 blur-3xl transition duration-1000",
@@ -23,94 +26,27 @@ export default function HomeWrapper() {
       {/* MAIN CONTENT: HERO SECTION */}
       <div className="flex h-[33%] w-[45%] text-slate-100/50">
         <div className="flex w-[55%] flex-col gap-[1.2rem] text-[1.2rem]">
-          {/* DESIGN TAB */}
-          <div
-            onPointerEnter={() => setHovered(!hovered)}
-            onPointerLeave={() => setHovered(!hovered)}
-            className="group relative flex flex-1 items-center justify-between rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/5 bg-gradient-to-r from-slate-900/90 to-transparent pl-8 transition duration-1000 ease-in-out hover:cursor-pointer hover:bg-gradient-to-r hover:from-indigo-950/15 hover:to-transparent hover:transition hover:duration-1000 hover:ease-in-out"
-          >
-            <div className="flex w-[7rem] items-end justify-between">
-              <div className="transition-colors duration-1000 ease-in-out group-hover:text-indigo-200/70 group-hover:transition-colors group-hover:duration-1000 group-hover:ease-in-out">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
-                  />
-                </svg>
+          {/* TABS: DESIGN, CODE, BUILD */}
+          {heroTabs.map(({ id, svgIcon, svgArrow, label, tag }) => (
+            <div
+              key={id}
+              onPointerEnter={() => setHovered(true)}
+              onPointerLeave={() => setHovered(false)}
+              onMouseEnter={() => setSelected(id)}
+              className="group relative flex flex-1 items-center justify-between rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/5 bg-gradient-to-r from-slate-900/90 to-transparent pl-8 transition duration-1000 ease-in-out hover:cursor-pointer hover:bg-gradient-to-r hover:from-indigo-950/15 hover:to-transparent hover:transition hover:duration-1000 hover:ease-in-out"
+            >
+              <div className="flex w-[7rem] items-end justify-between">
+                <span>{svgIcon}</span>
+                <h1 className="transition-colors duration-1000 ease-in-out group-hover:text-indigo-200/70 group-hover:transition-colors group-hover:duration-1000 group-hover:ease-in-out">
+                  {label}
+                </h1>
               </div>
-              <h1 className="transition-colors duration-1000 ease-in-out group-hover:text-indigo-200/70 group-hover:transition-colors group-hover:duration-1000 group-hover:ease-in-out">
-                DESIGN
-              </h1>
-            </div>
-            <div className="opacity-20 transition-all ease-in-out group-hover:text-indigo-200/70 group-hover:opacity-100 group-hover:transition-all group-hover:duration-1000 group-hover:ease-in-out">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-7"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            </div>
-          </div>
-          {/* CODE TAB */}
-          <div className="relative flex flex-1 items-center rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/5 bg-gradient-to-r from-slate-900/90 to-transparent pl-8">
-            <div className="flex w-[6rem] items-end justify-between">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
-                  />
-                </svg>
+              {hovered && selected === id ? <div>{tag}</div> : null}
+              <div className="opacity-20 transition-all ease-in-out group-hover:text-indigo-200/70 group-hover:opacity-100 group-hover:transition-all group-hover:duration-1000 group-hover:ease-in-out">
+                {svgArrow}
               </div>
-              <h1 className="">CODE</h1>
             </div>
-          </div>
-          {/* BUILD TAB */}
-          <div className="relative flex flex-1 items-center rounded-bl-lg rounded-tl-lg border-l-[1px] border-t-[1px] border-slate-50/5 bg-gradient-to-r from-slate-900/90 to-transparent pl-8">
-            <div className="flex w-[6rem] items-end justify-between">
-              <div className="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z"
-                  />
-                </svg>
-              </div>
-              <h1>BUILD</h1>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="mt-[-4rem] flex w-[45%] flex-1 flex-col items-start justify-start rounded-lg border-r-[1px] border-t-[1px] border-transparent bg-transparent pl-[4rem]">
           <div className="flex w-full items-center justify-between">
--- app/lib/heroTabs.js
+++ app/lib/heroTabs.js
@@ -1,18 +1,25 @@
 import DesignSVG from "../components/DesignSVG";
 import CodeSVG from "../components/CodeSVG";
 import BuildSVG from "../components/BuildSVG";
+import ArrowSVG from "../components/ArrowSVG";
 
 export const heroTabs = [
   {
     svgIcon: <DesignSVG />,
+    svgArrow: <ArrowSVG />,
     label: "Design",
+    tag: "UI/UX",
   },
   {
     svgIcon: <CodeSVG />,
+    svgArrow: <ArrowSVG />,
     label: "Code",
+    tag: "Nextjs/React",
   },
   {
     svgIcon: <BuildSVG />,
+    svgArrow: <ArrowSVG />,
     label: "Build",
+    tag: "Frontend Engineer",
   },
 ].map((n, idx) => ({ ...n, id: idx + 1 }));
