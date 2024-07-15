8bacf654dc4cb63de5550aacd30fd285048d945d
midCenturyDude7 <mgriffes@gmail.com>
2024-07-14 21:43:45 -0700

    Updated button styles

--- app/components/DesignContent.jsx
+++ app/components/DesignContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DesignContent() {
   return (
     <nav className="pl-[0.5rem] pt-[6.2rem]">
-      <ul className="flex items-center justify-between border-b border-slate-200/20 text-xl">
+      <ul className="flex items-center justify-between border-b border-slate-200/20 text-[1.175rem]">
         <li>
           <button className="rounded-tr-lg border-l-[1px] border-r-[1px] border-t-[1px] border-slate-200/20 px-4 py-[0.5rem] bg-indigo-950/15">
             Tailwindcss
