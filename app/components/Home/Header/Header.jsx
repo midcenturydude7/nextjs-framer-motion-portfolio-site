5ed0aef1e42635e601994471a9ae78a3a29fdd30
midCenturyDude7 <mgriffes@gmail.com>
2024-09-14 14:10:32 -0700

    Updated positioning

--- app/components/Home/Header/Header.jsx
+++ app/components/Home/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "./Logo/Logo";
 export default function Header() {
   return (
     <div className="flex items-center justify-between border-b-[1px] border-slate-200/10 px-28 py-[3rem] shadow-xl">
-      <Logo />
+      {/* <Logo /> */}
       <Menu />
       <Icons />
     </div>
