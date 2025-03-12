90a91aee78f7ee12696b8bf65b45973db0a0b1ef
midCenturyDude7 <mgriffes@gmail.com>
2025-03-11 21:06:20 -0700

    Updated text for project cards starting with log-in page

--- app/components/Home/Main/Projects/ProjectsContent.jsx
+++ app/components/Home/Main/Projects/ProjectsContent.jsx
@@ -3,13 +3,14 @@ import { projectItems } from "../../../../lib/projectItems";
 export default function ProjectsContent() {
   return (
     <div className="mt-16 w-[45%]">
-      <div className="flex flex-wrap justify-between items-center gap-8 text-[1.2rem] text-sky-100/50">
-        {projectItems.map(({ label, id }) => (
+      <div className="flex flex-wrap items-center justify-between gap-8 text-[1.2rem] text-sky-100/50">
+        {projectItems.map(({ label, id, description }) => (
           <div
             key={id}
-            className="flex h-[300px] w-[30%] min-w-72 flex-1 items-start justify-start rounded-lg border-t-[1px] border-slate-50/15 bg-gradient-to-b from-slate-900/90 to-transparent pl-6 pt-6"
+            className="flex h-[300px] w-[30%] min-w-72 flex-1 flex-col items-start justify-start rounded-lg border-t-[1px] border-slate-50/15 bg-gradient-to-b from-slate-900/90 to-transparent pl-6 pt-6"
           >
-            <h1>{label}</h1>
+            <h1 className="text-indigo-400 mb-3">{label}</h1>
+            <p className="pr-3">{description}</p>
           </div>
         ))}
       </div>
--- app/lib/projectItems.js
+++ app/lib/projectItems.js
@@ -1,6 +1,8 @@
 export const projectItems = [
   {
     label: "Login Page",
+    description:
+      "This is going to be a basic log-in page built in NextJs with a React frontend and a Supabase backend.",
   },
   {
     label: "AI Application",
